b027cf64ce6dc91953d87713f1314bdd73aff208
jhonquiceno <jonasquiceno@gmail.com>
2023-10-05 16:19:05 -0500

    Update index.js

--- src/components/mealPreviwGrid/index.js
+++ src/components/mealPreviwGrid/index.js
@@ -6,13 +6,8 @@ import './index.css';
 
 const MealPreviwGrid = () => {
 
-	const meals = [];
-
 	return (
 		<div className='meal-grid'>
-			{meals ? meals.map((meal) => (
-				<MealPreviw key={meal.idMeal} meal={meal} />
-			)): <h2>Search not found</h2>}
 		</div>
 	);
 };
